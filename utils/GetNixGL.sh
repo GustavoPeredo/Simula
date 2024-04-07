d9d4069d5cbc24e363b32b66280077a2369bf0c0
GustavoPeredo <gustavomperedo@protonmail.com>
2024-04-07 18:35:57 +0200

    Test building

--- utils/GetNixGL.sh
+++ utils/GetNixGL.sh
@@ -27,14 +27,14 @@ if [ ! -z $NIX_GL ]; then
     exit 0
 fi
 
-if [ -e /etc/NIXOS ]; then
-    echo ""
-elif [ -z $NVIDIA_VERSION ]; then
-    . $HOME/.nix-profile/etc/profile.d/nix.sh
-    installNixGL >/dev/null;
-    which nixGLIntel
-else
-    . $HOME/.nix-profile/etc/profile.d/nix.sh
-    installNixVulkanNvidia >/dev/null;
-    which nixVulkanNvidia
-fi
+#if [ -e /etc/NIXOS ]; then
+#    echo ""
+#elif [ -z $NVIDIA_VERSION ]; then
+#    . $HOME/.nix-profile/etc/profile.d/nix.sh
+#    installNixGL >/dev/null;
+#    which nixGLIntel
+#else
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+installNixVulkanNvidia >/dev/null;
+which nixVulkanNvidia
+#fi
